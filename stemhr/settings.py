ec11f02363b2f4a8acd7b6ef95b0dce57135e21b
Rosh <roshanibankar11@gmail.com>
2026-02-10 17:40:46 +0530

    hosts

--- stemhr/settings.py
+++ stemhr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY') #
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True' #
 
-ALLOWED_HOSTS = ['stemhr.pythonanywhere.com', '127.0.0.1', 'https://stem-hr-1.onrender.com']
+ALLOWED_HOSTS = ['stemhr.pythonanywhere.com', '127.0.0.1', 'stem-hr-1.onrender.com']
 
 
 # Application definition
